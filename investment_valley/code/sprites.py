a5b2f86e5a29b0db273cccb3360194d2438b7477
Jian Zhong <zhongjian089@gmail.com>
2023-11-26 04:44:20 -0800

    upload project sketch

--- investment_valley/code/sprites.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-class Generic(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
-        super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_rect(topleft = pos)
